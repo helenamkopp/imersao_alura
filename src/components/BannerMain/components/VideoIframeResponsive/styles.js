84060d9fd1891a52a89094b9b27beb30a6323fcc
Helena Kopp <helena.mkopp@gmail.com>
2020-07-28 17:50:50 -0300

    -

--- src/components/BannerMain/components/VideoIframeResponsive/styles.js
+++ src/components/BannerMain/components/VideoIframeResponsive/styles.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const VideoContainer = styled.div`
   position: relative;
   overflow: hidden;
+  border-radius: 10px;
   width: 100%;
   padding-top: 56.25%;
   @media (max-width: 800px) {
